1680d2f5ca1bdad26c0ab4250bb1d68382331cca
rmacdonald <rmacdonald@astro.cornell.edu>
2020-08-22 12:17:00 -0400

    New plotting function to compare cross sections

--- User.py
+++ User.py
@@ -21,15 +21,22 @@ import re
 import os
 
 P = 1    # Bar
-T = 2000   # K
+T_1 = 1200   # K
+T_2 = 2000   # K
 
 # Download line list
 Download_Line_List.summon(database=database, molecule=molecule)
 
 # Create cross section
-nu, sigma = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
-                                                    molecule = molecule, ionization_state = ionization, log_pressure = np.log10(P), temperature = T)
+nu_1, sigma_1 = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
+                                                    molecule = molecule, ionization_state = ionization, log_pressure = np.log10(P), temperature = T_1)
+
+nu_2, sigma_2 = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
+                                                    molecule = molecule, ionization_state = ionization, log_pressure = np.log10(P), temperature = T_2)
+
 
 # Plot cross section
-plot.plot_results(nu_arr = nu, sigma_arr = sigma, molecule = molecule, temperature = T, log_pressure = np.log10(P))
+#plot.plot_results(nu_arr = nu, sigma_arr = sigma, molecule = molecule, temperature = T, log_pressure = np.log10(P))
     
+plot.compare_cross_sections(molecule = molecule, label_1 = '1200 K', label_2 = '2000 K', 
+                            nu_arr_1 = nu_1, nu_arr_2 = nu_2, sigma_arr_1 = sigma_1, sigma_arr_2 = sigma_2)
