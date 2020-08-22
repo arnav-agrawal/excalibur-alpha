54e2b3e2e154d9631507e516574dbdec866d312b
rmacdonald <rmacdonald@astro.cornell.edu>
2020-08-22 12:27:45 -0400

    User.py test with two Na cross sections

--- User.py
+++ User.py
@@ -21,7 +21,7 @@ import re
 import os
 
 P = 1    # Bar
-T = 1200 
+#T = 1200 
 T_1 = 1200   # K
 T_2 = 2000   # K
 
@@ -29,12 +29,19 @@ T_2 = 2000   # K
 Download_Line_List.summon(database=database, molecule=molecule)
 
 # Create cross section
-nu, sigma = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
-                                                    molecule = molecule, ionization_state = ionization, 
-                                                    log_pressure = np.log10(P), temperature = T)
+#nu, sigma = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
+#                                                    molecule = molecule, ionization_state = ionization, 
+#                                                    log_pressure = np.log10(P), temperature = T)
 
 # Plot cross section
-plot.plot_results(nu_arr = nu, sigma_arr = sigma, molecule = molecule, temperature = T, log_pressure = np.log10(P))
+#plot.plot_results(nu_arr = nu, sigma_arr = sigma, molecule = molecule, temperature = T, log_pressure = np.log10(P))
     
-#plot.compare_cross_sections(molecule = molecule, label_1 = '1200 K', label_2 = '2000 K', 
-#                            nu_arr_1 = nu_1, nu_arr_2 = nu_2, sigma_arr_1 = sigma_1, sigma_arr_2 = sigma_2)
+nu_1, sigma_1 = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
+                                                    molecule = molecule, ionization_state = ionization, log_pressure = np.log10(P), temperature = T_1)
+
+nu_2, sigma_2 = Cthulhu_Refactored.create_cross_section(input_dir = '../input/', database = database, 
+                                                    molecule = molecule, ionization_state = ionization, log_pressure = np.log10(P), temperature = T_2)
+
+
+plot.compare_cross_sections(molecule = molecule, label_1 = '1200 K', label_2 = '2000 K', 
+                            nu_arr_1 = nu_1, nu_arr_2 = nu_2, sigma_arr_1 = sigma_1, sigma_arr_2 = sigma_2)
