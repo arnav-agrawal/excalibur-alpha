a2ea2c1f479871f14f1da873659b09c3c8896be2
arnav_agrawal <aa687@cornell.edu>
2020-08-24 14:18:53 -0400

    Changed name of project.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="excalibur-engine", # Replace with your own username
+    name="excalibur.engine", # Replace with your own username
     version="0.0.1",
     author="Arnav Agrawal",
     author_email="aa687@cornell.edu",
