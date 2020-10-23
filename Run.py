c42aa7f2712a7bb1776aac6d3f4ab65f697a66f2
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2020-10-23 03:17:15 -0400

    New core algorithm + multicore support

--- Run.py
+++ Run.py
@@ -7,15 +7,14 @@ from excalibur.core import compute_cross_section
 from excalibur.plot import plot_sigma_wl
 
 # Parameters
-species = 'CO'
+species = 'H2O'
 database = 'HITRAN'
 
 P = 1       # Pressure (bar)
-T = 1200    # Temperature (K)
+T = 1000    # Temperature (K)
 
 # Download line list
 summon(species = species, database = database)
-
 
 # Create cross section
 nu, sigma = compute_cross_section(input_dir = './input/', database = database, 
@@ -25,4 +24,3 @@ nu, sigma = compute_cross_section(input_dir = './input/', database = database,
 # Plot cross section
 plot_sigma_wl(nu_arr = nu, sigma_arr = sigma, species = species, temperature = T, 
               log_pressure = np.log10(P), database = database, plot_dir = './plots/')
-    
